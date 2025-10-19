e6c8a399106224c724d6a59e0a9cf6098e6284f1
sarvar2511 <ssaydaliev130@gmail.com>
2025-10-19 20:48:28 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import "@/styles/globals.css";
+/* _app.tsx fayl orqali barcha global integrationlarni amalga oshiramiz  */
+/* MUI ni integratsiyasi shu yerda amalga oshadi */
+
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import React, { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log(" APP COMPONENT -- -- PAGE Router");
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
 
-  return <Component {...pageProps} />;
+  //socket.io, Redux, MUI ....
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
+  /*Bu yerda biz MUI ni Component {...pageProps}
+  ga parent component sifatida wrap qilamiz 
+  bu degani page routing tizimidagi barcha fayllarga
+  tarqatmoqda degani */
 }
