ac4a1790a8e1ebe4661124d078ccc95f26b802d8
sarvar2511 <ssaydaliev130@gmail.com>
2025-10-19 19:07:32 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,33 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        {/* Search engine uchun instruction => 
+        web pageni search engine databasega index qilib ol 
+        va content dagi page laga follow qilishingiz mumkin degani
+        for GOOGLE searching uchun xizmat qiladi
+        */}
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/** SEO => Search Engine Optimization is below **/}
+        <meta
+          name="keyword "
+          content={" nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        {/* bu keywordlar google da search qlinganda shu kontentdagi
+         sozlar kiritilganda google tavsiya qiladi */}
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere and anytime in Korea. | " +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. | " +
+            "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
